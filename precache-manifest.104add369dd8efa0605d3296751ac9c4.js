4ffc1cf5d0dde0565ae627097bf11cd3218a04cb
Justice Otuya <jotuya2@gmail.com>
2019-07-11 18:49:31 +0100

    Updates

---
+++ precache-manifest.104add369dd8efa0605d3296751ac9c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559a105b33d44ad2d2bf8f1268221197",
+    "url": "/joyup_test/index.html"
+  },
+  {
+    "revision": "c07ebc00527fc90c595b",
+    "url": "/joyup_test/static/css/main.d1c88a41.chunk.css"
+  },
+  {
+    "revision": "365b832c97a87266a3ca",
+    "url": "/joyup_test/static/js/2.00e9be44.chunk.js"
+  },
+  {
+    "revision": "c07ebc00527fc90c595b",
+    "url": "/joyup_test/static/js/main.e2ad0c8a.chunk.js"
+  },
+  {
+    "revision": "6e8b7b32808544348bc4",
+    "url": "/joyup_test/static/js/runtime~main.9e89ffbb.js"
+  }
+]);
