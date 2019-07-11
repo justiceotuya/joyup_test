808a621bccb69f534d9d0fdedd1279acd4d80859
Justice Otuya <jotuya2@gmail.com>
2019-07-11 18:41:35 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1a8bf5da29f2263654cf09eb2ccb11a.js"
+  "/precache-manifest.d6d7ce2f95792fa9ed113ad3f5e48939.js"
 );
 
 self.addEventListener('message', (event) => {
