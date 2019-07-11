5ae611e2cfe0249909ed7758bcadd15637a7fff3
Justice Otuya <jotuya2@gmail.com>
2019-07-11 18:12:23 +0100

    Updates

---
+++ precache-manifest.a9e593db347d37edaa670e190ab807cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4576c76b414e2eb8b4804b286b15a0eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53742c56aa928fab4000",
+    "url": "/static/css/main.d1c88a41.chunk.css"
+  },
+  {
+    "revision": "365b832c97a87266a3ca",
+    "url": "/static/js/2.00e9be44.chunk.js"
+  },
+  {
+    "revision": "53742c56aa928fab4000",
+    "url": "/static/js/main.d62033e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
